2f8eb1df5480f1fb31e4758fab489e019f088fd4
AnasAblat <enasabelat@gmail.com>
2024-03-21 18:20:31 +0000

    deployment

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return <div>
     <h1>Hello World!</h1>
-    <p>Im on my new App</p>
+    <p>Im on my new App my first deployed app ever! that's cool</p>
+    <input type="text" placeholder="Enter Your name here!"></input>
   </div>
 }
 
